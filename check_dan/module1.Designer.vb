1cce8ab6634d5667386923c7c6b1651c34590406
David Lyons <lyons_ccl@hotmail.com>
2022-04-04 14:09:43 +0100

    Add project files.

---
+++ check_dan/module1.Designer.vb
@@ -0,0 +1,46 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataSet1 = New System.Data.DataSet()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataSet1
+        '
+        Me.DataSet1.DataSetName = "NewDataSet"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(408, 182)
+        Me.Name = "Form1"
+        Me.Text = "CHECK DAN DATABASE ( Orders, Surface,Thickness)"
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataSet1 As System.Data.DataSet
+
+End Class
---
+++ check_dan/module1.vb
@@ -0,0 +1,89 @@
+﻿
+Imports System.Data.OleDb
+Imports System.Net.Mail
+Imports System.IO
+Imports System.Data.SqlClient
+
+
+Module Module1
+
+    '  Dim info_file As String
+
+
+    Sub Main(ByVal feed() As String)
+        Dim au_num As String = feed(0)
+
+        'au_num = "'david','david-1.mht','16/03/2018','1','1_00mm_material','3','09/03/2018','6',.T.,'16/03/2018','GREEN','0'"
+
+        Dim stamp As Date
+        Dim timeStamp As Long
+        stamp = Now
+        timeStamp = CLng(stamp.Ticks)
+
+        au_num = timeStamp & "," & au_num
+
+        Try
+            update(au_num)
+        Catch
+        End Try
+
+        Try
+            updateSQL(au_num)
+        Catch
+        End Try
+
+
+leave_sub:
+
+    End Sub
+    Sub updateSQL(ByVal input As String)
+        REM ********************* DATABASE ********************************************
+        Dim status_table As String = "tiff_status"
+        Dim sqlCommand As String
+        sqlCommand = "INSERT INTO " & status_table & " (time_stamp,order_num,au_num,del_date,pool,thickness,surface,order_date,work_days,downloaded,del_ori,mask_color,large_order) VALUES (" & input & ")"
+
+
+        If InStr(sqlCommand, ".T.") Then
+            sqlCommand = sqlCommand.Replace(".T.", 1)
+        End If
+        If InStr(sqlCommand, ".F.") Then
+            sqlCommand = sqlCommand.Replace(".F.", 0)
+        End If
+
+        Dim id As Integer
+        id = Shell("S:\Job\in_house_software\OMS_Dev_SQL_update " & Chr(34) & SqlCommand & Chr(34))
+
+    End Sub
+    Sub update(ByVal input As String)
+        REM ********************* DATABASE ********************************************
+        Dim status_table As String = "t:\database3\tiff_status.dbf"
+        Dim oConnString As String = "Provider=VFPOLEDB.1;Data Source= " + status_table
+        Dim oCommandText As String
+
+        oCommandText = "INSERT INTO " & status_table & " (time_stamp,order_num,au_num,del_date,pool,thickness,surface,order_date,work_days,downloaded,del_ori,mask_color,large_order) VALUES (" & input & ")"
+
+        Dim omyConnection As New OleDbConnection(oConnString)
+
+        Try
+            omyConnection.Open()
+        Catch
+            GoTo close_and_leave
+        End Try
+
+        Try
+            Dim omyCommand As New OleDbCommand(oCommandText, omyConnection)
+            omyCommand.ExecuteNonQuery()
+        Catch
+            omyConnection.Close()
+        End Try
+
+close_and_leave:
+        omyConnection.Close()
+
+
+    End Sub
+
+End Module
+
+
+
